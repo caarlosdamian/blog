2c9ae06b9e5c95759d0f51d1e4a6f213cc9ec2c6
caarlosdamian <dapecarlos@gmail.com>
2022-10-21 18:21:33 -0500

    feat: addes home page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo3.png";
 
 export const Footer = () => {
   return (
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo3.png";
 import "../style.scss";
 
 export const Navbar = () => {
