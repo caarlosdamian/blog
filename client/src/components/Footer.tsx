4befc4218f5e94c00d0b7bdd2aaf5837e473548f
caarlosdamian <dapecarlos@gmail.com>
2022-10-20 19:21:30 -0500

    feat: added navbar styling

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={logo} alt="logoFooter" />
+      <span>Made with 💙 and <b>React.js</b>.</span>
+    </footer>
+  );
 };
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "../style.scss";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link to="/?cat=art" className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link to="/?cat=science" className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link to="/?cat=technology" className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link to="/?cat=cinema" className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link to="/?cat=desing" className="link">
+            <h6>DESING</h6>
+          </Link>
+          <Link to="/?cat=food" className="link">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Carlos</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link to="/write" className="link">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
